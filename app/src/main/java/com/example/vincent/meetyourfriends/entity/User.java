421ade1dbfb07707e45a8552942972f8b92ef94d
Vincent <vincent.bearpark@gmail.com>
2016-05-11 11:22:25 +0200

    orienté objet

--- app/src/main/java/com/example/vincent/meetyourfriends/entity/User.java
+++ app/src/main/java/com/example/vincent/meetyourfriends/entity/User.java
@@ -1,8 +1,78 @@
 package com.example.vincent.meetyourfriends.entity;
 
-/**
- * Created by acer-oem on 19.04.2016.
- */
 public class User {
 
+    private int id;
+    private String email;
+    private String password;
+    private String firstname;
+    private String lastname;
+    private String sexe;
+    private String birthdate;
+
+    public User(int id, String email, String password, String firstname, String lastname, String sexe, String birthdate) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.sexe = sexe;
+        this.birthdate = birthdate;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstname() {
+        return this.firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return this.lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getSexe() {
+        return this.sexe;
+    }
+
+    public void setSexe(String sexe) {
+        this.sexe = sexe;
+    }
+
+    public String getBirthdate() {
+        return this.birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
 }
--- app/src/main/java/com/example/vincent/meetyourfriends/entity/UserInEvent.java
+++ app/src/main/java/com/example/vincent/meetyourfriends/entity/UserInEvent.java
@@ -1,8 +1,38 @@
 package com.example.vincent.meetyourfriends.entity;
 
-/**
- * Created by acer-oem on 19.04.2016.
- */
 public class UserInEvent {
 
+    private int id;
+    private int idUser;
+    private int idEvent;
+
+    public UserInEvent(int id, int idUser, int idEvent) {
+        this.id = id;
+        this.idUser = idUser;
+        this.idEvent = idEvent;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdUser() {
+        return this.idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public int getIdEvent() {
+        return this.idEvent;
+    }
+
+    public void setIdEvent(int idEvent) {
+        this.idEvent = idEvent;
+    }
 }
