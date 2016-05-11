376d5783c1e26393f2773b426b0a27547f7e14dc
Daniel0693 <daniel_61993@hotmail.com>
2016-05-11 10:55:02 +0200

    Compléter les entités Commentaire et Event

--- app/src/main/java/com/example/vincent/meetyourfriends/entity/Commentaire.java
+++ app/src/main/java/com/example/vincent/meetyourfriends/entity/Commentaire.java
@@ -1,8 +1,47 @@
 package com.example.vincent.meetyourfriends.entity;
 
-/**
- * Created by acer-oem on 19.04.2016.
- */
 public class Commentaire {
 
+    private int id;
+    private String commentaire;
+    private int idUser;
+    private int idEvent;
+
+    public Commentaire() {
+
+    }
+    public Commentaire(int id, String commentaire, int idUser, int idEvent) {
+        this.id = id;
+        this.commentaire = commentaire;
+        this.idUser = idUser;
+        this.idEvent = idEvent;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCommentaire() {
+        return commentaire;
+    }
+    public void setCommentaire(String commentaire) {
+        this.commentaire = commentaire;
+    }
+
+    public int getIdUser() {
+        return idUser;
+    }
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public int getIdEvent() {
+        return idEvent;
+    }
+    public void setIdEvent(int idEvent) {
+        this.idEvent = idEvent;
+    }
 }
--- app/src/main/java/com/example/vincent/meetyourfriends/entity/Event.java
+++ app/src/main/java/com/example/vincent/meetyourfriends/entity/Event.java
@@ -1,8 +1,82 @@
 package com.example.vincent.meetyourfriends.entity;
 
-/**
- * Created by acer-oem on 19.04.2016.
- */
 public class Event {
+    private int id;
+    private String name;
+    private String description;
+    private String date;
+    private String time;
+    private String longitude;
+    private String latitude;
+    private int idUser;
 
+    public Event() {
+
+    }
+    public Event(int id, String name, String description, String date, String time, String longitude, String latitude, int idUser) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.date = date;
+        this.time = time;
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.idUser = idUser;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public int getIdUser() {
+        return idUser;
+    }
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
 }
